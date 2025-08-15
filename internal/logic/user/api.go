6282ba70a15e5c89aa678785cf7e412d61742ddf
Tyrfly <1071131161@qq.com>
2025-08-15 08:34:29 +0800

    feat: 更新用户认证逻辑，整合设备状态管理，优化认证数据结构

--- internal/logic/user/api.go
+++ internal/logic/user/api.go
@@ -1,8 +1,12 @@
 package user
 
 import (
+	"context"
 	"im-server/internal/repo"
+	"im-server/pkg/auth"
 	"im-server/pkg/protocol/pb/userpb"
+
+	"google.golang.org/protobuf/types/known/emptypb"
 )
 
 // UserIntService 模拟用户服务
@@ -14,4 +18,10 @@ type UserIntService struct {
 // NewUserIntService 创建一个新的 UserIntService 实例
 func NewUserIntService(queries *repo.Queries) *UserIntService {
 	return &UserIntService{queries: queries}
+}
+
+func (s *UserIntService) Auth(ctx context.Context, req *userpb.AuthRequest) (*emptypb.Empty, error) {
+	auth.AuthRepo.Get(req.UserId, req.DeviceId)
+	// 进行用户认证逻辑
+	return &emptypb.Empty{}, nil
 }
--- pkg/auth/auth.go
+++ pkg/auth/auth.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
-	"im-server/internal/repo"
 	"im-server/pkg/storage"
 	"strconv"
 )
@@ -15,19 +14,26 @@ type authRepo struct{}
 
 var AuthRepo = new(authRepo)
 
-func (*authRepo) Get(userID, deviceID uint64) (*repo.Device, error) {
+type AuthDevice struct {
+	DeviceID       uint64 `json:"device_id"`        // 设备ID
+	Token          string `json:"token"`            // 设备Token
+	TokenExpiresAt string `json:"token_expires_at"` // Token过期时间
+	Status         bool   `json:"status"`           // 设备状态，false：离线；	true：在线
+}
+
+func (*authRepo) Get(userID, deviceID uint64) (*AuthDevice, error) {
 	key := fmt.Sprintf(AuthKey, userID)
 	bytes, err := storage.RedisClient.HGet(context.Background(), key, strconv.FormatUint(deviceID, 10)).Bytes()
 	if err != nil {
 		return nil, err
 	}
 
-	var device repo.Device
+	var device AuthDevice
 	err = json.Unmarshal(bytes, &device)
 	return &device, err
 }
 
-func (*authRepo) Set(userID, deviceID uint64, device repo.Device) error {
+func (*authRepo) Set(userID, deviceID uint64, device AuthDevice) error {
 	bytes, err := json.Marshal(device)
 	if err != nil {
 		return err
@@ -38,14 +44,14 @@ func (*authRepo) Set(userID, deviceID uint64, device repo.Device) error {
 	return err
 }
 
-func (*authRepo) GetAll(userID uint64) (map[uint64]repo.Device, error) {
+func (*authRepo) GetAll(userID uint64) (map[uint64]AuthDevice, error) {
 	key := fmt.Sprintf(AuthKey, userID)
 	result, err := storage.RedisClient.HGetAll(context.Background(), key).Result()
 	if err != nil {
 		return nil, err
 	}
 
-	var devices = make(map[uint64]repo.Device, len(result))
+	var devices = make(map[uint64]AuthDevice, len(result))
 
 	for k, v := range result {
 		deviceID, err := strconv.ParseUint(k, 10, 64)
@@ -53,7 +59,7 @@ func (*authRepo) GetAll(userID uint64) (map[uint64]repo.Device, error) {
 			return nil, err
 		}
 
-		var device repo.Device
+		var device AuthDevice
 		err = json.Unmarshal([]byte(v), &device)
 		if err != nil {
 			return nil, err
