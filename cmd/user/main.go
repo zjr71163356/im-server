eecba826faebcad76fdbfd784b06f9a327efdeb3
Tyrfly <1071131161@qq.com>
2025-08-21 20:32:13 +0800

    feat: 添加gRPC用户服务的主程序文件

---
+++ cmd/user/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"im-server/internal/logic/user"
+	"im-server/pkg/config"
+	"im-server/pkg/protocol/pb/userpb"
+	"log/slog"
+	"net"
+
+	"google.golang.org/grpc"
+)
+
+func main() {
+
+	server := grpc.NewServer()
+	// pb.RegisterConnectServiceServer(server, &connect.ConnectService{})
+	listener, err := net.Listen("tcp", config.Config.Services.User.RPCAddr)
+	if err != nil {
+		panic(err)
+	}
+	userpb.RegisterUserIntServiceServer(server, &user.UserIntService{})
+	err = server.Serve(listener)
+	if err != nil {
+		slog.Error("serve error", "error", err)
+	}
+}
